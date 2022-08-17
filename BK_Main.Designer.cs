f1e45f75bf44708360a7cd5dd9f047f69565b700
Alexey <Alexey@DESKTOP-UNAR9FP>
2022-08-17 11:29:40 +0300

    Add project files.

---
+++ BK_Main.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BK_Driver
+{
+    partial class BK_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Cmd_Open = new System.Windows.Forms.Button();
+            this.Cmd_Close = new System.Windows.Forms.Button();
+            this.Cmd_Exit = new System.Windows.Forms.Button();
+            this.Cmd_Write = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.Cmd_Read = new System.Windows.Forms.Button();
+            this.Cmd_ONOFF = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(276, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Cmd_Open
+            // 
+            this.Cmd_Open.Enabled = false;
+            this.Cmd_Open.Location = new System.Drawing.Point(12, 52);
+            this.Cmd_Open.Name = "Cmd_Open";
+            this.Cmd_Open.Size = new System.Drawing.Size(107, 42);
+            this.Cmd_Open.TabIndex = 2;
+            this.Cmd_Open.Text = "Open";
+            this.Cmd_Open.UseVisualStyleBackColor = true;
+            this.Cmd_Open.Click += new System.EventHandler(this.Cmd_Open_Click);
+            // 
+            // Cmd_Close
+            // 
+            this.Cmd_Close.Enabled = false;
+            this.Cmd_Close.Location = new System.Drawing.Point(181, 52);
+            this.Cmd_Close.Name = "Cmd_Close";
+            this.Cmd_Close.Size = new System.Drawing.Size(107, 42);
+            this.Cmd_Close.TabIndex = 3;
+            this.Cmd_Close.Text = "Close";
+            this.Cmd_Close.UseVisualStyleBackColor = true;
+            this.Cmd_Close.Click += new System.EventHandler(this.Cmd_Close_Click);
+            // 
+            // Cmd_Exit
+            // 
+            this.Cmd_Exit.Location = new System.Drawing.Point(321, 380);
+            this.Cmd_Exit.Name = "Cmd_Exit";
+            this.Cmd_Exit.Size = new System.Drawing.Size(97, 42);
+            this.Cmd_Exit.TabIndex = 4;
+            this.Cmd_Exit.Text = "Exit";
+            this.Cmd_Exit.UseVisualStyleBackColor = true;
+            this.Cmd_Exit.Click += new System.EventHandler(this.Cmd_Exit_Click);
+            // 
+            // Cmd_Write
+            // 
+            this.Cmd_Write.Enabled = false;
+            this.Cmd_Write.Location = new System.Drawing.Point(181, 136);
+            this.Cmd_Write.Name = "Cmd_Write";
+            this.Cmd_Write.Size = new System.Drawing.Size(107, 42);
+            this.Cmd_Write.TabIndex = 5;
+            this.Cmd_Write.Text = "Write";
+            this.Cmd_Write.UseVisualStyleBackColor = true;
+            this.Cmd_Write.Click += new System.EventHandler(this.Cmd_Write_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Write:";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Enabled = false;
+            this.txtSend.Location = new System.Drawing.Point(53, 148);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(122, 20);
+            this.txtSend.TabIndex = 7;
+            this.txtSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtSend_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Read:";
+            // 
+            // txtRead
+            // 
+            this.txtRead.Enabled = false;
+            this.txtRead.Location = new System.Drawing.Point(15, 206);
+            this.txtRead.Multiline = true;
+            this.txtRead.Name = "txtRead";
+            this.txtRead.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRead.Size = new System.Drawing.Size(273, 168);
+            this.txtRead.TabIndex = 9;
+            // 
+            // Cmd_Read
+            // 
+            this.Cmd_Read.Enabled = false;
+            this.Cmd_Read.Location = new System.Drawing.Point(181, 380);
+            this.Cmd_Read.Name = "Cmd_Read";
+            this.Cmd_Read.Size = new System.Drawing.Size(107, 42);
+            this.Cmd_Read.TabIndex = 10;
+            this.Cmd_Read.Text = "Read";
+            this.Cmd_Read.UseVisualStyleBackColor = true;
+            this.Cmd_Read.Click += new System.EventHandler(this.Cmd_Read_Click);
+            // 
+            // Cmd_ONOFF
+            // 
+            this.Cmd_ONOFF.Enabled = false;
+            this.Cmd_ONOFF.Location = new System.Drawing.Point(311, 136);
+            this.Cmd_ONOFF.Name = "Cmd_ONOFF";
+            this.Cmd_ONOFF.Size = new System.Drawing.Size(107, 42);
+            this.Cmd_ONOFF.TabIndex = 11;
+            this.Cmd_ONOFF.Text = "ON";
+            this.Cmd_ONOFF.UseVisualStyleBackColor = true;
+            this.Cmd_ONOFF.Click += new System.EventHandler(this.Cmd_ONOFF_Click);
+            // 
+            // BK_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 431);
+            this.Controls.Add(this.Cmd_ONOFF);
+            this.Controls.Add(this.Cmd_Read);
+            this.Controls.Add(this.txtRead);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Cmd_Write);
+            this.Controls.Add(this.Cmd_Exit);
+            this.Controls.Add(this.Cmd_Close);
+            this.Controls.Add(this.Cmd_Open);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "BK_Main";
+            this.Text = "BK Driver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Cmd_Open;
+        private System.Windows.Forms.Button Cmd_Close;
+        private System.Windows.Forms.Button Cmd_Exit;
+        private System.Windows.Forms.Button Cmd_Write;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.Button Cmd_Read;
+        private System.Windows.Forms.Button Cmd_ONOFF;
+    }
+}
+
---
+++ BK_Main.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Ivi.Visa;
+using NationalInstruments.Visa;
+using Ivi.Visa.Interop;
+
+/*****************************  SCN20 ATE TEST STATION *****************************
+ * BK 9130C Driver 
+ * Written by Alexey Tseirif
+ * 8/08/2022
+ * Function list:
+ *  1. Initialization             - bool BK_Init() 
+ *  2. Setup OVP,                 - void Setup_OVP(int Channel, string Voltage)
+ *  3. Output each channel On/Off - optional   
+ *  4. Output all channels On/Off - void Turn_ONOFF(bool ON_OFF, int channels)
+ *  5. Measure Voltage           - float Meas_Voltage(int Channel)
+ *  6. Measure Current           - float Meas_Current(int Channel)
+ *  7. Setup Voltage by channel  - void Setup_Voltage(int Channel, string Voltage)
+ *  8. Setup Current by channel  - void Setup_Current(int Channel, string Current)
+ *  
+ *  
+ * ********************************************************************************* 
+*/
+namespace BK_Driver
+{
+    public partial class BK_Main : Form
+    {
+        Ivi.Visa.Interop.ResourceManager rMgr = new ResourceManagerClass();//Create a resource manager
+        FormattedIO488 src = new FormattedIO488Class();
+
+        public string sendstring; // strings
+        public string src_address = string.Empty;
+
+        public BK_Main()
+        {
+            InitializeComponent();
+            getAvailableResources();
+        }
+        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Cmd_Open.Enabled = true;
+            Cmd_Close.Enabled = true;
+            src_address = comboBox1.SelectedItem.ToString();
+        }
+        public void getAvailableResources()//function that find the available USBTMC resources and populates combobox1
+        {
+            try
+            {
+                //USB VISA resources
+                string[] resources = rMgr.FindRsrc("USB?*");
+                comboBox1.Items.AddRange(resources);
+            }
+            catch (Exception)
+            {
+                txtRead.Text = "No Resources Available";
+
+            }
+        }
+        private void Cmd_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Cmd_Open_Click(object sender, EventArgs e)
+        {
+            comboBox1.Enabled = false;
+            txtSend.Enabled = true; Cmd_Read.Enabled = true;
+            txtRead.Enabled = true; Cmd_Write.Enabled = true; Cmd_ONOFF.Enabled = true;
+
+            string srcAddress = src_address;
+
+            src.IO = (IMessage)rMgr.Open(srcAddress, AccessMode.NO_LOCK, 2000, "");
+            src.IO.Timeout = 2000;
+        }
+        private void Cmd_Close_Click(object sender, EventArgs e)
+        {
+            src.IO.Close();
+            comboBox1.Enabled = true;
+            Cmd_Close.Enabled = false;
+            Cmd_ONOFF.Enabled = false;
+        }
+        private void Cmd_Write_Click(object sender, EventArgs e)
+        {
+
+            sendstring = sendstring.Replace("\r\n","\n");
+            src.WriteString(sendstring);//("*IDN?\n");//
+            txtRead.AppendText(txtSend.Text + "\r\n");
+            txtSend.Text = string.Empty;
+        }
+        private void Cmd_Read_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtRead.Text += src.ReadString() + "\r\n";
+            }
+            catch (TimeoutException)
+            {
+                txtRead.Text = "timeout exception";
+
+            }
+
+        }
+        // function turn_ONOFF get two parameters (status ON/OFF, number of channels to be turned ON/OFF)
+        // turn_ONOFF(true, 2) - channel 1 and 2 must be turned ON
+        private void Turn_ONOFF(bool ON_OFF, int channels)
+        {
+            string b_command = string.Empty;
+            if (ON_OFF)
+            {
+                switch (channels)
+                {
+                    case 1:
+                        b_command = "1,0,0";
+                        break;
+                    case 2:
+                        b_command = "1,1,0";
+                        break;
+                    case 3:
+                        b_command = "1,1,1";
+                        break;
+                }
+            }
+            else 
+            {
+                switch (channels)
+                {
+                    case 1:
+                        b_command = "0,1,1";
+                        break;
+                    case 2:
+                        b_command = "0,0,1";
+                        break;
+                    case 3:
+                        b_command = "0,0,0";
+                        break;
+                }
+
+            }
+            sendstring = "APP:OUT "+ b_command + "\n";
+            src.WriteString(sendstring);
+        }
+        //Setup voltage (channel, voltage)
+        //Setup_Voltage(2, 15) - setup channel 2 voltage 15V
+        private void Setup_Voltage(int Channel, string Voltage)
+        {
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            b_command = "SOUR:VOLT:LEV:IMM:AMPL " + Voltage + "\n";
+            src.WriteString(b_command);
+
+        }
+        //Setup current (channel, current)
+        //Setup_Current(3, 1) - setup channel 3 current 1A
+        private void Setup_Current(int Channel, string Current)
+        {
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            b_command = "SOUR:CURR:LEV:IMM:AMPL " + Current + "\n";
+            src.WriteString(b_command);
+
+        }
+        //Setup_OVP (channel, voltage)
+        //Setup_OVP(3, 10) - setup channel 3 voltage 10V maximum limit
+        private void Setup_OVP(int Channel, string Voltage)
+        {
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            b_command = "SOUR:VOLT:PROT:LEV " + Voltage + "V\n";
+            src.WriteString(b_command);
+
+        }
+        //Measurement Voltage
+        //Meas_Voltage(2) - measurement voltage of channel 2
+        private float Meas_Voltage(int Channel)
+        {
+            float Meas_Voltage = 0;
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            b_command = "MEAS:SCAL:VOLT:DC?\n";
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            Meas_Voltage = float.Parse(src.ReadString());
+
+            return Meas_Voltage;
+        }
+        //Measurement Current
+        //Meas_Current(1) - measurement current of channel 1
+        private float Meas_Current(int Channel)
+        {
+            float Meas_Current = 0;
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            b_command = "MEAS:SCAL:CURR:DC?\n";
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            Meas_Current = float.Parse(src.ReadString());
+
+            return Meas_Current;
+        }
+
+        //Initilization function return of (*IDN?\n) -> (B&K Precision, 9130C, 800885011777110054, 1.11-1.04)
+        private bool BK_Init()
+        {            
+            string p_answer = string.Empty;
+            bool BK_INIT = false;
+                        
+            src.WriteString("*IDN?\n");
+            System.Threading.Thread.Sleep(200);
+            
+            p_answer = src.ReadString();
+            BK_INIT = p_answer.Contains("800885011777110054");
+
+            return BK_INIT;
+        }
+        private void Turn_ONOFF_EACH(bool ON_OFF, int Channel)
+        {
+            string b_command = string.Empty;
+
+            switch (Channel)
+            {
+                case 1:
+                    b_command = "INST CH1\n";
+                    break;
+                case 2:
+                    b_command = "INST CH2\n";
+                    break;
+                case 3:
+                    b_command = "INST CH3\n";
+                    break;
+            }
+            src.WriteString(b_command);
+            System.Threading.Thread.Sleep(200);
+            if (ON_OFF)
+            {
+                b_command = "SOUR:CHAN:OUTP ON\n";
+            }
+            else
+            {
+                b_command = "SOUR:CHAN:OUTP OFF\n";
+            }
+            sendstring = b_command;
+            src.WriteString(sendstring);
+        }
+
+        private void TxtSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                sendstring = txtSend.Text+"\n";
+                Cmd_Write.Focus();
+            }
+        }
+
+        private void Cmd_ONOFF_Click(object sender, EventArgs e)
+        {
+            //sendstring = "SYST:VERS?"+"\n";
+            //sendstring = "OUTP?" + "\n";
+            //src.WriteString(sendstring);            
+            Setup_Voltage(2, "10");
+            //Turn_ONOFF(true, 1);
+            Turn_ONOFF_EACH(true, 2);
+            //float gt = Meas_Voltage(1);
+            //bool ts = BK_Init();
+        }
+    }
+}
